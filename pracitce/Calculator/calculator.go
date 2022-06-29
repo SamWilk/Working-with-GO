7da8acb80889a40b50c929f59e100da81042041c
SamWilk <samwilk18@yahoo.com>
2022-06-29 15:40:30 -0400

    Updating calculator.go

--- pracitce/Calculator/calculator.go
+++ pracitce/Calculator/calculator.go
@@ -11,19 +11,42 @@ import (
 func main() {
 	fmt.Println("Hello from calculator")
 	//Getting User Input
-	input1 := assignValue()
-	input2 := assignValue()
+	input1 := AssignValue()
+	input2 := AssignValue()
 
+	result := GetOp(&input1, &input2)
+	fmt.Println("Result:", result, "for" , input1, "and", input2)
 }
 
-func assignValue() int {
+func GetOp(value1 ,value2 *int) int{
+Restart:
+	fmt.Print("Operator: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	switch input = strings.TrimSpace(input); input {
+	case "*":
+		return *value1 * *value2
+	case "+":
+		return *value1 + *value2
+	case "-":
+		return *value1 - *value2
+	case "/":
+		return *value1 / *value2
+	default:
+		fmt.Println("Error,", input, "is not allowed")
+		goto Restart
+	}
+}
+
+func AssignValue() int {
+Restart:
 	fmt.Print("Value: ")
 	reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
 	temp, err := strconv.Atoi(strings.TrimSpace(input))
 	if err != nil {
 		fmt.Println(err)
-		panic("Entry Not allowed")
+		goto Restart
 	}
 	return temp
 }
